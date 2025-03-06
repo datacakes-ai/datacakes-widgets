11844aa1f4259fbe8c2a4dfe7225255218378acf
rsarvis <rsarvis@gmail.com>
2025-03-06 16:48:06 -0500

    legacy backend, test site

--- cubie/src/core/config/main.ts
+++ cubie/src/core/config/main.ts
@@ -1,6 +1,6 @@
 import process from 'process'
 
-export const DATACAKES_CODE_SERVICE_URL = process.env.CODE_SVC_URL ? process.env.CODE_SVC_URL : 'https://api.datacakes.ai';
+export const DATACAKES_CODE_SERVICE_URL = process.env.CODE_SVC_URL ? process.env.CODE_SVC_URL : 'https://api-legacy.datacakes.ai';
 
 export const MAIN_SITE_URL = 'https://www.datacakes.ai'
 
